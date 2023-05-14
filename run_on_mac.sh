4a20229dadec0547e6f3539ddffe307d4d2a5b15
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-05-14 16:48:08 -0600

    Cleaning up the mac run script

--- run_on_mac.sh
+++ run_on_mac.sh
@@ -2,4 +2,6 @@
 
 # With thanks https://prateeknischal.github.io/posts/i-c-and-so-does-rust/
 
-env RUSTFLAGS="-L/opt/homebrew/lib/" cargo run -- -l --sdr1serial 00013305 --sdr1freqs 133.69 134.420 --sdr1decoding-type acars
+echo "Running the command: cargo run --" "$@"
+
+env RUSTFLAGS="-L/opt/homebrew/lib/" cargo run -- "$@"
