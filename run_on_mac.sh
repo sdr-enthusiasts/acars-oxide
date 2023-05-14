31d4ee70438ba913e49df1e9088c26cfe1c24c0c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-05-14 11:01:04 -0600

    bump cargo versions to latest

--- run_on_mac.sh
+++ run_on_mac.sh
@@ -2,4 +2,4 @@
 
 # With thanks https://prateeknischal.github.io/posts/i-c-and-so-does-rust/
 
-env RUSTFLAGS="-L/opt/homebrew/lib/" cargo run -- -l --sdr1serial 00013305 --sdr1gain 46 --sdr1ppm 1 --sdr1freqs 13300000 --sdr1decoding-type acars --sdr1biastee true
+env RUSTFLAGS="-L/opt/homebrew/lib/" cargo run -- -l --sdr1serial 00013305  --sdr1freqs 13300000 --sdr1decoding-type acars
