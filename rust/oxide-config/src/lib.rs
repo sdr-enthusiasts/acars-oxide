f8d65d5bc778abeec374ffafc39e031904888e63
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-05-21 14:55:52 -0600

    add copyrights

--- rust/oxide-config/src/lib.rs
+++ rust/oxide-config/src/lib.rs
@@ -19,6 +19,22 @@ const MAX_GAIN: f32 = 60.0;
     about,
     long_about = "ACARS Oxide is a program that allows you to receive and decode ACARS and VDLM2 messages."
 )]
+// Copyright (C) 2023  Fred Clausen
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 pub struct OxideInput {
     /// General Program Options
     /// Set the log level. debug, trace, info are valid options. Info is default.
