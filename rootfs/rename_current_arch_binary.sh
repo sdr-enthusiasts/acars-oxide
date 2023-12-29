80822461afe256a005ed88bb1c6095a90e231c02
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2023-12-28 23:23:04 -0700

    see why it's failing

--- rootfs/rename_current_arch_binary.sh
+++ rootfs/rename_current_arch_binary.sh
@@ -5,11 +5,11 @@ set -x
 ls -la /opt/
 
 # determine which binary to keep
-if /opt/acars-oxide.amd64 --version > /dev/null 2>&1; then
+if /opt/acars-oxide.amd64 --version; then
   mv -v /opt/acars-oxide.amd64 /opt/acars-oxide
-elif /opt/acars-oxide.arm64 --version > /dev/null 2>&1; then
+elif /opt/acars-oxide.arm64 --version; then
   mv -v /opt/acars-oxide.arm64 /opt/acars-oxide
-elif /opt/acars-oxide.armv7 --version > /dev/null 2>&1; then
+elif /opt/acars-oxide.armv7 --version; then
   mv -v /opt/acars-oxide.armv7 /opt/acars-oxide
 else
   echo >&2 "ERROR: Unsupported architecture"
