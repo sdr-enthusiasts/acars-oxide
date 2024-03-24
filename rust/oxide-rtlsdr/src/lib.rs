e6e36460f6cf20195f26f134313d3a207e8dad91
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2024-03-24 01:48:58 -0600

    updates

--- rust/oxide-rtlsdr/src/lib.rs
+++ rust/oxide-rtlsdr/src/lib.rs
@@ -325,6 +325,7 @@ impl RtlSdr {
 
             Some(mut reader) => loop {
                 let read = reader.read(&mut buffer).await.unwrap();
+                println!("Read {} bytes", read);
 
                 let mut bytes_iterator = buffer.iter().take(read);
 
